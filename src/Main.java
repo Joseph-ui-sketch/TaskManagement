3bd2802a941e477a8437829a21d8cb7dcc667a5c
hernajos000 <hernajos000@warren.k12.in.us>
2024-11-08 13:55:18 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Dishes", Priority.MEDIUM));
+        tasks.add(new Task("Food", Priority.HIGH));
+        tasks.add(new Task("Play Game", Priority.LOW));
+        tasks.add(new Task("Sport", Priority.MEDIUM));
+       // tasks.sort();
+
+        for (Task task : tasks) {
+            task.displayDetails();
+        }
+
+    }
+}
